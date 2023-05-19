9c78575422681f905e58f03b60486ebdbc1597d5
Jahid Hasan <jh3495689@gmail.com>
2023-05-19 12:17:00 +0600

    added a subcategory data

--- index.js
+++ index.js
@@ -13,6 +13,56 @@ app.get('/', (req, res) => {
     res.send('port is running on server')
 })
 
+//actionToys
+//ZKIt4hB1iIGCdsJH
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://actionToys:ZKIt4hB1iIGCdsJH@cluster0.rxkguw5.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toysCollection = client.db("actionDB").collection("toys")
+
+        app.get('/toys', async (req, res) => {
+            const cursor = await toysCollection.find().toArray()
+            res.send(cursor)
+        })
+
+        app.get('/toys/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await toysCollection.findOne(query)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on PORT ${port}`)
